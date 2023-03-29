e0f62098327a5dc47723104f3be81602e7e95d02
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-29 16:01:35 +0530

    Value

--- src/main/java/com/schoolexam/SchoolexamApplication.java
+++ src/main/java/com/schoolexam/SchoolexamApplication.java
@@ -1,14 +1,23 @@
 package com.schoolexam;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 @SpringBootApplication
+@CrossOrigin()
 public class SchoolexamApplication {
-
+	@Value("${spring.application.name}")
+	private String name;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolexamApplication.class, args);
 	}
-
+	@GetMapping(value = "/")
+	   public String name() {
+	      return name;
+	   }
 }
